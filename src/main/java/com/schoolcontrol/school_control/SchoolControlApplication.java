0ad7a8e484365498aab0ba80407023ee23955fd6
Vanderlei Soares de Oliveira <vanderlei.oliveira@e-deploy.com.br>
2025-01-10 10:22:58 +0000

    upload files new

---
+++ src/main/java/com/schoolcontrol/school_control/SchoolControlApplication.java
@@ -0,0 +1,13 @@
+package com.schoolcontrol.school_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schoolcontrol/school_control/SchoolControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolcontrol.school_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
